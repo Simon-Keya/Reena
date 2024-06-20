a35cd63df34c69113cb01be6ba0991c540c300e3
Simon Keya <120686819+Simon-Keya@users.noreply.github.com>
2024-06-20 04:41:13 -0700

    commit_1

---
+++ Reena/src/test/java/com/restaurantmanagement/Reena/ReenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantmanagement.Reena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
